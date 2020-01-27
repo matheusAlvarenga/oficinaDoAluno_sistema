7a63a82c33cd9b94ec65d6b57c8ce9178380063c
Matheus Alvarenga de Oliveira <matheus.alvarenga@aluno.ufabc.edu.br>
2020-01-27 16:37:09 -0200

    1

---
+++ login.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+	<title></title>
+</head>
+<body style="background: url('_imagens/loading.gif') center no-repeat; background-color: #4b525d;">
+<h1 align="center">Carregando...</h1>
+
+<?php 
+
+$login = $_POST['login'];
+$senha = $_POST['senha'];
+
+
+
+?>
+
+</body>
+</html>
